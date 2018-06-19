c3c33e0d24ff4825ef51cee273381ae797bbdf2d
b2blosangeles <b2blosangeles@gmail.com>
2018-06-18 21:42:21 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 let room = req.query['room'];
 pkg.fs.readFile(env.site_path + '/tpl/index.html', 'utf-8', function(err, content) {
-	res.send(content.replace(/\$room/ig, room));
+	res.send(content.replace(/\{\$room\}/ig, room));
 });
