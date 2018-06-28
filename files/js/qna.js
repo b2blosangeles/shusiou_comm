9e54e26c17c87740422504fb1f0535e8dfdc1963
b2blosangeles <b2blosangeles@gmail.com>
2018-06-28 16:45:26 -0700

    Update qna.js

--- files/js/qna.js
+++ files/js/qna.js
@@ -9,12 +9,12 @@
 		}
 		this.client = function(incomeData) {			
 		}		
-		this.init = function(_proxy) {
+		this.init = function(_socket, _link, _proxy) {
 			let me = this;
 			me.ping_id = {};
 			me._proxy = _proxy;
 			
-			me.socket = io.connect(_dns.comm[0]);
+			me.socket = io.connect(_link);
 			me.socket.on('connect', function() {
 				me.socket.on('serverData', function(incomeData) {
 					if (incomeData.data._code === 'clientRequest') {
@@ -27,7 +27,7 @@
 				setInterval(function() {
 					let ping_id = new Date().getTime();
 					me.ping_id[ping_id] = 1;
-					me.socket.emit('clientData', {_socket: _socket, _link: _dns.comm[0], _proxy: _proxy, 
+					me.socket.emit('clientData', {_socket: _socket, _link: _link, _proxy: _proxy, 
 					data: {_sender: me.socket.id, _code : 'qnaRequest', ping_id: ping_id}});
 					document.getElementById('income_info').innerHTML =  JSON.stringify(me.ping_id);
 					me.audit();
@@ -46,49 +46,3 @@
 	};
 	window.QNA = obj;
 })();
-
-/*
-		let qna = {ping_id:{}};
-		
-		qna.server = function(incomeData) {
-			me.socket.emit('clientData', {_socket: incomeData.data._sender, _link: incomeData._link, 
-				_proxy: _proxy, 
-				data: {connection: [socket.id, incomeData.data._sender], _code : 'resQnaRequest',
-				      ping_id : incomeData.data.ping_id
-				      }});			
-		}
-		qna.client = function(incomeData) {			
-		}		
-		qna.init = function() {
-			let me = this;
-			me.socket = io.connect(_dns.comm[0]);
-			me.socket.on('connect', function() {
-				me.socket.on('serverData', function(incomeData) {
-					if (incomeData.data._code === 'clientRequest') {
-						me.server(incomeData);
-					} else {
-						delete me.ping_id[incomeData.data.ping_id];
-						me.client(incomeData);
-					}
-				});
-				setInterval(function() {
-					let ping_id = new Date().getTime();
-					me.ping_id[ping_id] = 1;
-					me.socket.emit('clientData', {_socket: _socket, _link: _dns.comm[0], _proxy: _proxy, 
-					data: {_sender: me.socket.id, _code : 'qnaRequest', ping_id: ping_id}});
-					document.getElementById('income_info').innerHTML =  JSON.stringify(me.ping_id);
-					me.audit();
-				}, 500);
-			});			
-		};
-		qna.audit = function() {
-			let me = this;
-			for (var k in me.ping_id) {
-				if ((new Date().getTime() - k) > 2000) {
-					me.socket.close();
-					window.close();
-				}
-			};
-		};		
-		qna.init();
-*/
