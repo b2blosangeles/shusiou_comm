8bdff36e4859890a50787ac63a1628e35fc19e40
b2blosangeles <b2blosangeles@gmail.com>
2018-06-29 10:30:37 -0700

    Update qna.js

--- files/js/qna.js
+++ files/js/qna.js
@@ -24,7 +24,7 @@
 		}
 		this.sendToClient = function(data, socket_id) {
 			let me = this;
-			data._sender = socket_id;
+			data._sender = me.socket_id;
 			console.log('--sending to client');
 			me.socket.emit('clientData', {_socket: socket_id, _link: me.cfg.link, _proxy: me.cfg.proxy, 
 				data: data});		
