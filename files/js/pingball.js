bcbb234aaa95c0605ee5c3b2864835bca1a96548
b2blosangeles <b2blosangeles@gmail.com>
2018-08-20 15:18:58 -0700

    Update pingball.js

--- files/js/pingball.js
+++ files/js/pingball.js
@@ -28,7 +28,7 @@
 		};
 		this.audit = function(ta) {
 			for (var o in ta.q) {
-				if (new Date().getTime() -  ta.q[o].tm > me.timeOut) {
+				if (new Date().getTime() -  ta.q[o].tm > ta.timeOut) {
 					console.log('Timeout ' + o + ':');
 					console.log(ta.q[o]);
 					delete(ta.q[o]);
