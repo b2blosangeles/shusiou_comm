0f96e1452d593c44d1b23711c6677921870b125e
b2blosangeles <b2blosangeles@gmail.com>
2018-08-21 15:29:16 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
-let room = req.query['room'],
+let room = req.query['room'], type = req.query['type'],
     socket = req.query['socket'];
-pkg.fs.readFile(env.site_path + '/tpl/index.html', 'utf-8', function(err, content) {
-	res.send(content.replace(/\{\$room\}/ig, room).replace(/\{\$socket\}/ig, socket));
-});
+if (type === 'SR') {
+	pkg.fs.readFile(env.site_path + '/tpl/SR.html', 'utf-8', function(err, content) {
+		res.send(content.replace(/\{\$room\}/ig, room).replace(/\{\$socket\}/ig, socket));
+	});	
+} else {
+	pkg.fs.readFile(env.site_path + '/tpl/index.html', 'utf-8', function(err, content) {
+		res.send(content.replace(/\{\$room\}/ig, room).replace(/\{\$socket\}/ig, socket));
+	});
+}
